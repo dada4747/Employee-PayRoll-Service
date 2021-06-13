a39b685f3cd62dfb0db822790b0cfc7c987ba0a6
dada4747 <adsurerahul96@gmail.com>
2021-06-13 23:47:02 +0530

    Welcome to Employee Payroll Service

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+    public enum IOService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+    }
+    private List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
+    public EmployeePayrollService(){}
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollDataList) {
+        this.employeePayrollDataList = employeePayrollDataList;
+    }
+
+    private void readEmployeePayrollData(Scanner sc){
+        System.out.println("Enter Employee ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter employee name: ");
+        String name = sc.nextLine();
+        System.out.println("Enetr Employee salary: ");
+        double salary = sc.nextDouble();
+        employeePayrollDataList.add(new EmployeePayrollData(id, name, salary));
+
+    }
+    private void writeEmployeePayrollData(){
+        System.out.println("Writing employee payroll to console\n" + employeePayrollDataList);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollDataList = new ArrayList<EmployeePayrollData>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollDataList);
+        Scanner sc = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(sc);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+}
